6bdf73b1ea3d07237c8a28d7e90c500e6227cb80
DESKTOP-3TGS524\Benjamin <benjamin.le.roux.lfp@gmail.com>
2017-03-11 10:24:57 +0100

    first commit

---
+++ Assets/Scripts/ProjectileDragging.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileDragging : MonoBehaviour {
+    public float maxStretch = 3.0f;
+    public LineRenderer catapultLineFront;
+    public LineRenderer catapultLineBack;
+
+    private SpringJoint2D spring;
+    private Rigidbody2D rb;
+    private Transform catapult;
+    private Ray rayToMouse;
+    private Ray leftCatapultToProjectile;
+    private bool clickedOn;
+    private float maxStretchSqr;
+    private float circleRadius;
+    private Vector2 prevVelocity;
+
+    private void Awake()
+    {
+        spring = GetComponent<SpringJoint2D>();
+        rb = GetComponent<Rigidbody2D>();
+        catapult = spring.connectedBody.transform;
+    }
+
+    private void Start()
+    {
+        LineRendererSetup();
+        rayToMouse = new Ray(catapult.position, Vector3.zero);
+        leftCatapultToProjectile = new Ray(catapultLineFront.transform.position, Vector3.zero);
+        maxStretchSqr = maxStretch * maxStretch;
+        CircleCollider2D circle = GetComponent<CircleCollider2D>();
+        circleRadius = circle.radius;
+    }
+
+    private void Update()
+    {
+        if (clickedOn)
+        {
+            Dragging();
+        }
+        if (spring != null)
+        {
+            if (!rb.isKinematic && prevVelocity.sqrMagnitude > rb.velocity.sqrMagnitude)
+            {
+                Destroy(spring);
+                rb.velocity = prevVelocity;
+            }
+
+            if (!clickedOn)
+            {
+                prevVelocity = rb.velocity;
+            }
+
+            LineRendererUpdate();
+        }
+        else
+        {
+            catapultLineFront.enabled = false;
+            catapultLineBack.enabled = false;
+        }
+    }
+
+    private void LineRendererSetup()
+    {
+        catapultLineFront.SetPosition(0, catapultLineFront.transform.position);
+        catapultLineBack.SetPosition(0, catapultLineBack.transform.position);
+
+        catapultLineFront.sortingLayerName = "Catapult";
+        catapultLineBack.sortingLayerName = "Catapult";
+
+        catapultLineFront.sortingOrder = 3;
+        catapultLineBack.sortingOrder = 1;
+    }
+
+    private void OnMouseDown()
+    {
+        spring.enabled = false;
+        clickedOn = true;
+    }
+
+    private void OnMouseUp()
+    {
+        spring.enabled = true;
+        rb.isKinematic = false;
+        clickedOn = false;
+    }
+
+    private void Dragging()
+    {
+        Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 catapultToMouse = mouseWorldPoint - catapult.position;
+
+        if (catapultToMouse.sqrMagnitude > maxStretchSqr)
+        {
+            rayToMouse.direction = catapultToMouse;
+            mouseWorldPoint = rayToMouse.GetPoint(maxStretch);
+        }
+
+        mouseWorldPoint.z = 0f;
+        transform.position = mouseWorldPoint;
+    }
+
+    private void LineRendererUpdate()
+    {
+        Vector2 catapultToProjectile = transform.position - catapultLineFront.transform.position;
+        leftCatapultToProjectile.direction = catapultToProjectile;
+        Vector3 holdPoint = leftCatapultToProjectile.GetPoint(catapultToProjectile.magnitude + circleRadius);
+        catapultLineFront.SetPosition(1, holdPoint);
+        catapultLineBack.SetPosition(1, holdPoint);
+    }
+}
---
+++ Assets/Scripts/TargetDamage.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDamage : MonoBehaviour {
+
+    public int hitPoints = 2;
+    public Sprite damageSprite;
+    public float damageImpactSpeed;
+
+    private int currentHitPoints;
+    private float damageImpactSpeedSqr;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        currentHitPoints = hitPoints;
+        damageImpactSpeedSqr = damageImpactSpeed * damageImpactSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag != "Damager") { return; }
+
+        if (collision.relativeVelocity.sqrMagnitude < damageImpactSpeedSqr) { return; }
+
+        spriteRenderer.sprite = damageSprite;
+        currentHitPoints--;
+
+        if (currentHitPoints <= 0) { Kill(); }
+    }
+
+    private void Kill()
+    {
+        spriteRenderer.enabled = false;
+        Collider2D collider = gameObject.GetComponent<Collider2D>();
+        Rigidbody2D rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        collider.enabled = false;
+        rigidbody.isKinematic = true;
+    }
+}
